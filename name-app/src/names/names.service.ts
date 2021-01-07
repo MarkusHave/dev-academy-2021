0e76513b506145b8795ab1948558eeb80ec9f3d3
Markus Haverinen <markushave@gmail.com>
2021-01-07 15:58:57 +0200

    add sorting by amount

--- name-app/src/names/names.service.ts
+++ name-app/src/names/names.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { NamesDTO } from './names.dto';
+import { names } from '../names.json';
 
 @Injectable()
-export class NamesService {}
+export class NamesService {
+  // Returns all names and amounts, ordered by amount, most popular first
+  getAllByAmount(): Array<NamesDTO> {
+    return names.sort((a, b) => (a.amount > b.amount ? -1 : 1));
+  }
+}
